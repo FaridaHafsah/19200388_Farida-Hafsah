bec56cc912f1fd5e528dba121525deaa72e6760f
19200388_Farida Hafsah <90598417+FaridaHafsah@users.noreply.github.com>
2021-09-21 15:17:50 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. Yuk kita belajar Web Programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
